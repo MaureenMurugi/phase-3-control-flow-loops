4f2263b13249344ecc22e7ba834075071464c2f4
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-10-19 15:53:06 +0300

    commit tree f056e596b8edfe17da460f75a787a4202c692ebb

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  year = 10
+  while year >0
+    puts year
+    year -=1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,27 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |i|
+   if i % 3 == 0 && i % 5 == 0
+     puts "FizzBuzz"
+   elsif i % 3 == 0
+     puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+   end
+  end
 end
 
 def reverse_string(str)
   # your code here
+
+  i =-1
+  word = ""
+  while i > -str.length-1
+    word+=str[i]
+    i-=1
+  end
+  word
 end
